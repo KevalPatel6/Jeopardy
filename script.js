59689c8a50bd0de33aab58a69f964dfc35812f63
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-08 10:15:24 -0400

    created landing page and event listner to toggle hide to show instructions and start button

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-
-<<<<<<< HEAD
 console.log("jason")
-=======
-
 console.log("Keval");
 console.log("John");
->>>>>>> 6de02ab049b081adbb49c7d7b8d885e3efa9b6da
+
+let startButtonEl = document.getElementById('startButtonEl');
+let titleEl = document.getElementById('titleEl');
+
+startButtonEl.addEventListener('click', startGame);
+
+function startGame() {
+    console.log("working!")
+
+        titleEl.classList.toggle("hide");
+        startButtonEl.classList.toggle("hide");
+        instructionsTitleEl.classList.toggle("hide");
+        instructionsEl.classList.toggle("hide");
+        beginGameEl.classList.toggle("hide");
+}
+
+// ?start the game event listener goes to where
+
