e4f177df50b08bf0b4058f990070eee63de4118f
[Keval Patel] <[khp90611@gmail.com]>
2023-08-08 13:38:18 -0400

    Saving checkpoint, trying to get questions and answers to save to local storage based on categoryID

--- gameboard-layout/gameboard.js
+++ gameboard-layout/gameboard.js
@@ -1,64 +1,93 @@
 //------------------------------Make answer choices buttons----------------------------------//
-let allAnswersEl = document.querySelectorAll('button')
+let allAnswersEl = document.querySelector('button')
 let categories = document.querySelectorAll('.categories')
-
-
-
 let randomCategoriesID = JSON.parse(localStorage.getItem('categories')) || [];
-//----------------------------Jason's Question Page fetch-------------------------------------------------------//
+let qNA= JSON.parse(localStorage.getItem('questions')) || [];
+
 let randCat = JSON.stringify(Math.random() * 20000);
-let randNum = (Math.random()* 5);
+let randNum = (Math.random() * 5);
 
 
-getRandomCategories()
 
-function getRandomCategories(){
-    
-    fetch(`https://jservice.io/api/categories?count=10&offset=${randCat}`)
-    .then(response => response.json())
-    .then(catData =>{
-       
-        for (let i = 0; i < catData.length; i++) {
-           if(catData[i].clues_count>=5){
-            pushAndSave(catData[i].id)
-           }
-            
-        }
-    
+clearLocalStorage();
+getRandomCategories();
+getQuestionsAndAnswers();
 
-})}
 
-displayCategories();
 
-function displayCategories(x) {
-    let categoryIDNumber = JSON.parse(localStorage.getItem('categories'))
-    console.log(categoryIDNumber)
-    for (let i = 0; i < 5; i++) {
-        console.log(categoryIDNumber[i])
-        fetch(`https://jservice.io/api/category?id=${categoryIDNumber[i]}`)
-        .then(function(response){
-            return response.json()
+function getQuestionsAndAnswers(){
+    let CategoryIDNumber = JSON.parse(localStorage.getItem('categories'))
+
+    for (let i = 0; i < array.length; i++) {   
+    fetch(`https://jservice.io/api/clues?category=${categoryIDNumber[i]}`)
+        .then(response => response.json())
+        .then(QnAData => {
+            console.log(qNA)
+            pushAndSave(qNA, QnAData, 'questions')
         })
-        .then(function(categoriesData){
-            console.log(categoriesData)
-            
-            
-            categories[i].textContent = categoriesData.title.toUpperCase();
-        })
+        
     }
-}
-
-function pushAndSave(x){
-    randomCategoriesID.push(x)
-    localStorage.setItem('categories', JSON.stringify(randomCategoriesID))
+//Working---------here//
 }
 
 
+
+
+
+
+
+function getRandomCategories() {
+
+    fetch(`https://jservice.io/api/categories?count=10&offset=${randCat}`)
+        .then(response => response.json())
+        .then(catData => {
+
+            for (let i = 0; i < catData.length; i++) {
+                if (catData[i].clues_count >= 5) {
+                    console.log(catData[i])
+                    pushAndSave(randomCategoriesID,catData[i],'categories')
+                    
+                }
+
+            }
+
+            displayCategories();
+        })
+}
+
+
+
+function displayCategories() {
+    let categoryIDNumber = JSON.parse(localStorage.getItem('categories'))
+    for (let i = 0; i < 5; i++) {
+        fetch(`https://jservice.io/api/category?id=${categoryIDNumber[i].id}`)
+            .then(function (response) {
+                return response.json()
+            })
+                .then(function (categoriesData) {
+                    categories[i].textContent = categoriesData.title.toUpperCase();
+                })
+            }}
+        
+    
+
+
+function pushAndSave(x, y, z) {
+    x.push(y)
+    localStorage.setItem(z, JSON.stringify(x))
+}
+
+function clearLocalStorage() {
+    localStorage.clear()
+}
 //fetch using the id in local storage
 
 //---------------------------------Assign fetch category to appropriate place------------------//
-
-
+// allAnswersEl.addEventListener('click', function(event){
+//     if(event.target==='button'){
+//         window.location.href='../Questions_Page/questionsPage.html'
+//     }
+// })
 
 
 
