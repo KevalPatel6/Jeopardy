4bfe0b5925433b049d3e3529b9cb2b91b6201b0d
godot23 <jasonanavon@gmail.com>
2023-08-08 11:08:55 -0400

    trying to troubleshoot

--- Questions_Page/questionsPage.js
+++ Questions_Page/questionsPage.js
@@ -3,28 +3,31 @@ let answerText = document.querySelector("#answer");
 let timerText = document.querySelector("#timer");
 let pointsText = document.querySelector("#points");
 let categoryText = document.querySelector("#category");
-
 let randCat = JSON.stringify(Math.random() * 20000);
 let randNum = (Math.random()* 5);
 let question;
-let answer;
+let answerStorage;
 let category;
 let points;
+
 fetch(`https://jservice.io/api/categories?count=1&offset=${randCat}`)
     .then(response => response.json())
     .then(catData =>{
         let catValue = catData[0].id;
         let category = catData[0].title;
+        categoryText.textContent = category;
         console.log(catValue);
         return fetch(`https://jservice.io/api/category?id=${catValue}`);
     })
     .then(response => response.json())
     .then(data =>{
-        answer = data.clues[0].answer;
+        console.log(JSON.stringify(data));
+        let answer = data.clues[0].answer;
         question = data.clues[0].question;
         points = data.clues[0].value;
         console.log(points, question, answer);
-    })
-pointsText.textContent = points;
-questionText.textContent = question;
 
+        pointsText.textContent = points;
+        questionText.textContent = question;
+        answerStorage = JSON.stringify(answer);
+    })
