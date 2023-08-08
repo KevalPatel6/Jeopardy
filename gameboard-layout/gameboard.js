41c5b278b232850fca2922038b17339415f38fa6
[Keval Patel] <[khp90611@gmail.com]>
2023-08-08 10:46:01 -0400

    added Jason's fetch code to my javascript. Now I need to display cat onto screen and save the id to local storage for him to pull

--- gameboard-layout/gameboard.js
+++ gameboard-layout/gameboard.js
@@ -1,22 +1,33 @@
 //------------------------------Make answer choices buttons----------------------------------//
 let allAnswersEl = document.querySelectorAll('button')
+let categories = document.querySelectorAll('.categories')
 
 
 
 
+//----------------------------Jason's Question Page fetch-------------------------------------------------------//
+let randCat = JSON.stringify(Math.random() * 20000);
+let randNum = (Math.random()* 5);
+let question;
+let answer;
+let category;
+let points;
 
-
-
-
-
-//----------------------------fetch categories-------------------------------------------------------//
-
-fetch(`https://jservice.io/api/categories/`)
-    .then(function(response){
-        response.json()
+fetch(`https://jservice.io/api/categories?count=1&offset=${randCat}`)
+    .then(response => response.json())
+    .then(catData =>{
+        let catValue = catData[0].id;
+        let category = catData[0].title;
+        console.log(catData);
+        return fetch(`https://jservice.io/api/category?id=${catValue}`);
     })
-    .then(function(categories){
-        console.log(categories)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        answer = data.clues[0].answer;
+        question = data.clues[0].question;
+        points = data.clues[0].value;
+        console.log(points, question, answer);
     })
 
 
