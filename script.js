e85e77e447870fa7907e55ff2f35452254646633
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-08-08 14:13:10 -0400

    styled and linked landing page, instructions page and endpage

--- script.js
+++ script.js
@@ -5,22 +5,27 @@ console.log("John");
 let startButtonEl = document.getElementById('startButtonEl');
 let titleEl = document.getElementById('titleEl');
 
-startButtonEl.addEventListener('click', startGame);
+startButtonEl.addEventListener('click', nextPage);
 
-function startGame() {
+function nextPage() {
     console.log("working!")
 
-        titleEl.classList.toggle("hide");
-        startButtonEl.classList.toggle("hide");
-        instructionsTitleEl.classList.toggle("hide");
-        instructionsEl.classList.toggle("hide");
-        beginGameEl.classList.toggle("hide");
+    titleEl.classList.toggle("hide");
+    startButtonEl.classList.toggle("hide");
+    buttonDiv.classList.toggle("hide");
+    instructionsTitleEl.classList.toggle("hide");
+    instructionsEl.classList.toggle("hide");
+    beginGameEl.classList.toggle("hide");
 }
 
 // ?start the game event listener goes to Keval's game board
 
+let beginGameEl = document.getElementById('beginGameEl');
 
-console.log("jason")
+document.getElementById("beginGameEl").addEventListener('click', startGame);
 
-console.log("Keval");
-console.log("John");
+function startGame() {
+    console.log(window.location.href);
+    console.log("working!")
+    window.location.href = "/gameboard.html";
+}
